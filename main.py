38a93edb5826c836910ec9455365ba9229fc8d41
Kellan <KellanJones@gmail.com>
2025-10-19 22:04:10 -0400

    Added ship movement

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    pygame.init
+    #start message to terminal
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    #initialize pygame, create the screen, and set up the clock
+    pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    BLACK = (0, 0, 0)
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill(BLACK)
+
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen) 
         pygame.display.flip()
+
+        #limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
